78c1ee9cdc086650b2615ec6080801e7aa1e0239
Abhishek Mali <154997025+maliabhi23@users.noreply.github.com>
2025-06-29 11:07:40 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,17 +1,16 @@
-import mysql from 'mysql2';
-import dotenv from 'dotenv';
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
 dotenv.config();
 
-const connectionUrl = process.env.MYSQL_URL;
-
-const db = mysql.createConnection(connectionUrl);
-
-db.connect((err) => {
-  if (err) {
-    console.error('❌ MySQL connection failed:', err.message);
-  } else {
-    console.log('✅ Connected to Railway MySQL');
-  }
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-export default db;
+module.exports = db;
